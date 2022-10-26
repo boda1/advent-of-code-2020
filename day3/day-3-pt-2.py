3409dc1187dad3fd7ee40a05bf2cb28744c5c6d4
Adam Boda <adam.boda@UK1022-F403TM.local>
2022-10-26 08:37:55 +0100

    days 3 and the beginning of 4

--- day3/day-3-pt-2.py
+++ day3/day-3-pt-2.py
@@ -1,23 +1,25 @@
 routeFile = open('./input.txt')
 routes = routeFile.readlines()
 currentPosition = 0
+squaresRight = 1
 allSquares = []
 numberOfTrees = 0
 
 
-for i in range(len(routes)):
+for i in range(0, len(routes), 2):
     if currentPosition <= 30:
         allSquares = allSquares + [routes[i][currentPosition]]
-        currentPosition += 3
+        currentPosition += squaresRight
     elif currentPosition > 30:
         currentPosition = currentPosition - 31
         allSquares = allSquares + [routes[i][currentPosition]]
-        currentPosition += 3
+        currentPosition += squaresRight
+
 
 for j in range(len(allSquares)):
     if allSquares[j] == '#':
         numberOfTrees += 1
 
+
 print(allSquares)
 print(numberOfTrees)
-
