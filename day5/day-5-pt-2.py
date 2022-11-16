c11026894dc5aa0f65e1c651d4e80199d57ad4bb
Adam Boda <adam.boda@UK1022-F403TM.local>
2022-11-16 08:27:07 +0000

    finished day 5 part 2

--- day5/day-5-pt-2.py
+++ day5/day-5-pt-2.py
@@ -6,8 +6,9 @@ current_row_list = []
 number_of_columns = 8
 initial_columns_list = [i for i in range(number_of_columns)]
 current_column_list = []
-seat_id = []
+seat_ids_with_tickets = []
 highest_seat_value = 0
+my_seat = 0
 
 
 with open('./input.txt') as f:
@@ -24,11 +25,35 @@ with open('./input.txt') as f:
                 current_column_list = current_column_list[:len(current_column_list)//2]
             elif char == 'R':
                 current_column_list = current_column_list[len(current_column_list)//2:]
-        seat_id.append(current_row_list[0] * 8 + current_column_list[0])
+        seat_ids_with_tickets.append(current_row_list[0] * 8 + current_column_list[0])
 
 
-for value in seat_id:
+for value in seat_ids_with_tickets:
     if value > highest_seat_value:
         highest_seat_value = value
 
-print("Highest seat ID is: ", highest_seat_value)
+print("All seat with tickets: ", seat_ids_with_tickets)
+
+
+# part 2: find the missing boarding pass to identify my seat
+
+"""
+
+Generate complete list of IDs for 128x8 seats
+Compare with input list provided
+Find the values present in complete list that are missing from input list
+
+"""
+
+
+all_seat_ids = [row * 8 + column for row in initial_rows_list for column in initial_columns_list]
+missing_seats = [seat if seat not in seat_ids_with_tickets and seat + 1 in seat_ids_with_tickets and seat - 1 in seat_ids_with_tickets else None for seat in all_seat_ids]
+
+
+for seat in missing_seats:
+    if seat is not None:
+        my_seat = seat
+
+print("My seat number is: ", my_seat)
+
+
